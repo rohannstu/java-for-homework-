7f82bed99ba078652996223982a06ff964630c0a
Rohan Sinha <105815725+rohannstu@users.noreply.github.com>
2022-10-21 20:50:41 +0600

    Create code4.java

---
+++ code4.java
@@ -0,0 +1,11 @@
+//floating point arithmatic
+class MyClass
+{
+    public static void main(String args[])
+    {
+       float a = 20.5F, b= 6.4F;  
+       System.out.println("a = " +  a*a); 
+
+        
+    }
+}
